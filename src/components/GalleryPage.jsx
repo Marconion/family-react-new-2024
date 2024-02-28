078cf75083e2793ab3f9860bf94863e559e03e5d
Marconion <zujovic.marko@gmail.com>
2024-02-28 22:48:53 +0100

    fix

--- src/components/GalleryPage.jsx
+++ src/components/GalleryPage.jsx
@@ -18,155 +18,155 @@ import { useEffect } from "react";
 export const GalleryPage = () => {
   const itemData = [
     {
-      img: "../images/gallery-img/Family-1.jpg",
+      img: "./images/gallery-img/Family-1.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-2.jpg",
+      img: "./images/gallery-img/Family-2.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-3.jpg",
+      img: "./images/gallery-img/Family-3.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-4.jpg",
+      img: "./images/gallery-img/Family-4.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-5.jpg",
+      img: "./images/gallery-img/Family-5.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-6.jpg",
+      img: "./images/gallery-img/Family-6.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-7.jpg",
+      img: "./images/gallery-img/Family-7.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-8.jpg",
+      img: "./images/gallery-img/Family-8.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-9.jpg",
+      img: "./images/gallery-img/Family-9.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-10.jpg",
+      img: "./images/gallery-img/Family-10.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-11.jpg",
+      img: "./images/gallery-img/Family-11.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-12.jpg",
+      img: "./images/gallery-img/Family-12.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-13.jpg",
+      img: "./images/gallery-img/Family-13.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-14.jpg",
+      img: "./images/gallery-img/Family-14.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-15.jpg",
+      img: "./images/gallery-img/Family-15.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-16.jpg",
+      img: "./images/gallery-img/Family-16.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-17.jpg",
+      img: "./images/gallery-img/Family-17.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-18.jpg",
+      img: "./images/gallery-img/Family-18.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-19.jpg",
+      img: "./images/gallery-img/Family-19.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-20.jpg",
+      img: "./images/gallery-img/Family-20.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-21.jpg",
+      img: "./images/gallery-img/Family-21.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-22.jpg",
+      img: "./images/gallery-img/Family-22.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-23.jpg",
+      img: "./images/gallery-img/Family-23.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-24.jpg",
+      img: "./images/gallery-img/Family-24.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-25.jpg",
+      img: "./images/gallery-img/Family-25.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-26.jpg",
+      img: "./images/gallery-img/Family-26.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-27.jpg",
+      img: "./images/gallery-img/Family-27.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-28.jpg",
+      img: "./images/gallery-img/Family-28.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-29.jpg",
+      img: "./images/gallery-img/Family-29.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-30.jpg",
+      img: "./images/gallery-img/Family-30.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-31.jpg",
+      img: "./images/gallery-img/Family-31.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-32.jpg",
+      img: "./images/gallery-img/Family-32.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-33.jpg",
+      img: "./images/gallery-img/Family-33.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-34.jpg",
+      img: "./images/gallery-img/Family-34.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-35.jpg",
+      img: "./images/gallery-img/Family-35.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-36.jpg",
+      img: "./images/gallery-img/Family-36.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-37.jpg",
+      img: "./images/gallery-img/Family-37.jpg",
       title: "img",
     },
     {
-      img: "../images/gallery-img/Family-38.jpg",
+      img: "./images/gallery-img/Family-38.jpg",
       title: "img",
     },
   ];
