7a23663a5e08dd84a9d901c7f3729f254e83b1ee
Marconion <zujovic.marko@gmail.com>
2024-02-29 15:46:56 +0100

    text ispravka

--- src/components/Onama.jsx
+++ src/components/Onama.jsx
@@ -45,39 +45,39 @@ export const Onama = () => {
         <Divider />
         <Typography variant="h8" textAlign={"center"}>
           Family event place je restoran namenjen organizovanju svih vrsta
-          porodicnih proslava od 30 do 150 gostiju. U mogucnosti smo da
-          organizujemo sve vrste proslava, od decijih rodjendana i krstenja do
+          porodičnih proslava od 30 do 150 gostiju. U mogućnosti smo da
+          organizujemo sve vrste proslava, od dečijih rodjendana i krštenja do
           svadbi i korporativnih proslava.
           <br />
           <br />
-          Smesten je na samoj ivici Kosutnjaka, na osuncanoj padini koja pruza
-          fantastican pogled od kojeg zastaje dah.
+          Smešten je na samoj ivici Košutnjaka, na osunčanoj padini koja pruža
+          fantastičan pogled od kojeg zastaje dah.
           <br />
           <br />
-          Family restoran je jedinstven prostor za slavlja u gradu jer Vam pruza
-          mogucnost izbora u kakvom cete okruzenju organizovati vas svecani dan.
+          Family restoran je jedinstven prostor za slavlja u gradu jer Vam pruža
+          mogućnost izbora u kakvom ćete okruženju organizovati vas svečani dan.
           <br />
           <br />
-          Na raspolqganju su vam dve moderno opremljene zatvorene sale. Velika
+          Na raspolaganju su vam dve moderno opremljene zatvorene sale. Velika
           sala je kapaciteta do 100 gostiju, a mala sala je kapaciteta do 50
-          gostiju. Sale su medjusobno povezane sto omogucava i organizaciju
-          vecih  proslava do 150 gostiju.
+          gostiju. Sale su medjusobno povezane što omogućava i organizaciju
+          većih proslava do 150 gostiju.
           <br />
           <br />
-          Ono sto vam je takodje na raspolaganju, a nas razlikuje od drugih
-          ptostora ovog tipa su i otvorena terasa sa ocaravajucim pogledom na
-          Avalu, kapaciteta do 50 gostiju. Kruna nase ponude je nasa zelena
-          oaza, nas vrt, kapaciteta do 50 gostiju.
+          Ono što vam je takodje na raspolaganju, a nas razlikuje od drugih
+          prostora ovog tipa su i otvorena terasa sa očaravajućim pogledom na
+          Avalu, kapaciteta do 50 gostiju. Kruna naše ponude je naša zelena
+          oaza, naš vrt, kapaciteta do 50 gostiju.
           <br />
           <br />
-          Ovaj prostor je potpuno opremljen za hedonisticko uzivanje sa osecajem
+          Ovaj prostor je potpuno opremljen za hedonističko uživanje sa osećajem
           bega iz gradske vreve.
           <br />
           <br />
           Za najmladje i najbitnije goste Family event place je spremio posebno
-          iznenadjenje. Na raspolaganju vam je potpuno opremljena decija
-          igraonica od 70m2, sa profesionalnim animatorima koji ce im svaki
-          trenutak proveden u nasem restoranu uciniti nezaboravnim.
+          iznenadjenje. Na raspolaganju vam je potpuno opremljena dečija
+          igraonica od 70m2, sa profesionalnim animatorima koji će im svaki
+          trenutak proveden u našem restoranu učiniti nezaboravnim.
         </Typography>
         <Divider />
         <Stack spacing={3} direction={direction}>
